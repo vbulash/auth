cc0bbf1e59cd525161da6f41226f844d2c7c9027
Валерий Булаш <v.bulash@ansgar.ru>
2024-10-22 11:14:52 +0300

    Реализация ДЗ № 2 - очередной подход

---
+++ database/migrations/00001_create_user_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE users;
+-- +goose StatementEnd
